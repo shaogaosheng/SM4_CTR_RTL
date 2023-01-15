6494e272bd06b344298e8af22f9719f6d6e885b4
shaogaosheng <shaogaosheng@gmail.com>
2023-01-15 19:42:03 +0800

    key exp module finished but not sim

---
+++ sm4_key.v
@@ -0,0 +1,150 @@
+module SM4_KEY (
+    input             clk_sys,
+    input             sys_rst_n,
+
+    input             sm4_start,
+    input    [127:0]  sm4_key_in,
+    input             sm4_key_in_vld,
+
+    output   [31:0]   key2core_rkey,
+    output            key2core_rkey_vld
+);
+
+localparam IDLE          = 1'b0;
+localparam KEY_EXPANSION = 1'b1;
+
+localparam FK0 = 32'ha3b1bac6;
+localparam FK1 = 32'h56aa3350;
+localparam FK2 = 32'h677d9197;
+localparam FK3 = 32'hb27022dc;
+
+wire [31:0] sm4_key_in_0part; //sm4_key_in[127:96]
+wire [31:0] sm4_key_in_1part; //sm4_key_in[95:64]
+wire [31:0] sm4_key_in_2part; //sm4_key_in[63:32]
+wire [31:0] sm4_key_in_3part; //sm4_key_in[31:0]
+
+reg [31:0] sm4_key_0part; //sm4_key[127:96], exor(sm4_key_in[127:96], FK0)
+reg [31:0] sm4_key_1part; //sm4_key[95:64]
+reg [31:0] sm4_key_2part; //sm4_key[63:32]
+reg [31:0] sm4_key_3part; //sm4_key[31:0]
+
+reg [4:0] sm4_round_cnt;
+reg fsm_curr_state;
+reg fsm_next_state;
+
+wire [31:0]  sm4_key_cki;
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        sm4_round_cnt <= 5'd0;
+    end
+    else if (fsm_curr_state == IDLE) begin
+        sm4_round_cnt <= sm4_round_cnt + 1'b1;
+    end
+    else ;
+end
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        fsm_curr_state <= 1'b0;
+    end
+    else begin
+        fsm_curr_state <= fsm_next_state;
+    end
+end
+
+always @(*)
+begin
+    case (fsm_curr_state)
+        IDLE: 
+        if ((sm4_start == 1'b1) && (sm4_key_in_vld == 1'b1)) begin
+            fsm_next_state = KEY_EXPANSION;
+        end 
+        else begin
+            fsm_next_state = IDLE;
+        end
+        default:        //KEY_EXPANSION state;
+        if (sm4_round_cnt == 5'd31) begin
+            fsm_next_state = IDLE;
+        end
+        else begin
+            fsm_next_state = KEY_EXPANSION;
+        end
+    endcase
+end
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        sm4_key_0part <= 32'd0;
+    end
+    else if ((sm4_start == 1'b1) && (sm4_key_in_vld == 1'b1)) begin
+        sm4_key_0part <= sm4_key_in_0part ^ FK0;
+    end
+    else if (fsm_curr_state == KEY_EXPANSION) begin
+        sm4_key_0part <= sm4_key_1part;
+    end
+    else ;
+end
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        sm4_key_1part <= 32'd0;
+    end
+    else if ((sm4_start == 1'b1) && (sm4_key_in_vld == 1'b1)) begin
+        sm4_key_1part <= sm4_key_in_1part ^ FK1;
+    end
+    else if (fsm_curr_state == KEY_EXPANSION) begin
+        sm4_key_1part <= sm4_key_2part;
+    end
+    else ;
+end
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        sm4_key_2part <= 32'd0;
+    end
+    else if ((sm4_start == 1'b1) && (sm4_key_in_vld == 1'b1)) begin
+        sm4_key_2part <= sm4_key_in_2part ^ FK2;
+    end
+    else if (fsm_curr_state == KEY_EXPANSION) begin
+        sm4_key_2part <= sm4_key_3part;
+    end
+    else ;
+end
+
+always @(posedge clk_sys)
+begin
+    if (sys_rst_n == 1'b0) begin
+        sm4_key_3part <= 32'd0;
+    end
+    else if ((sm4_start == 1'b1) && (sm4_key_in_vld == 1'b1)) begin
+        sm4_key_3part <= sm4_key_in_3part ^ FK3;
+    end
+    else if (fsm_curr_state == KEY_EXPANSION) begin
+        sm4_key_3part <= sm4_rkey_out;
+    end
+    else ;
+end
+
+assign sm4_key_exp_in = {sm4_key_0part,sm4_key_1part,sm4_key_2part,sm4_key_3part};
+assign key2core_rkey = sm4_rkey_out;
+
+
+SM4_KEY_CKI U_SM4_KEY_CKI(
+    .clk_sys(clk_sys),
+    .sm4_round_cnt(sm4_round_cnt),
+    .sm4_key_cki(sm4_key_cki)
+);
+
+SM4_KEY_EXP_ONE_ROUND U_SM4_KEY_EXP_ONE_ROUND(
+    .sm4_key_exp_in(sm4_key_exp_in),
+    .sm4_key_cki(sm4_key_cki),
+    .sm4_rkey_out(sm4_rkey_out)
+);
+    
+endmodule
---
+++ sm4_key_exp_one_round.v
@@ -0,0 +1,69 @@
+module SM4_KEY_EXP_ONE_ROUND(
+    input         [127 : 0]    sm4_key_exp_in,
+    input         [31  : 0]    sm4_key_cki,
+    output   reg  [31  : 0]    sm4_rkey_out
+);
+
+wire [31:0] sm4_key_exp_in_0part;
+wire [31:0] sm4_key_exp_in_1part;
+wire [31:0] sm4_key_exp_in_2part;
+wire [31:0] sm4_key_exp_in_3part;
+
+wire [31:0] sm4_key_exp_sbox_in;
+wire [31:0] sm4_key_exp_sbox_out;
+
+wire [7:0]  sm4_key_exp_sbox_in_0part;
+wire [7:0]  sm4_key_exp_sbox_in_1part;
+wire [7:0]  sm4_key_exp_sbox_in_2part;
+wire [7:0]  sm4_key_exp_sbox_in_3part;
+
+wire [7:0]  sm4_key_exp_sbox_out_0part;
+wire [7:0]  sm4_key_exp_sbox_out_1part;
+wire [7:0]  sm4_key_exp_sbox_out_2part;
+wire [7:0]  sm4_key_exp_sbox_out_3part;
+
+wire [31:0] sm4_key_exp_sbox_out_left_shift_13bit;
+wire [31:0] sm4_key_exp_sbox_out_left_shift_23bit;
+wire [31:0] sm4_key_exp_shift_out;
+
+assign    { sm4_key_exp_in_0part,
+            sm4_key_exp_in_1part,
+            sm4_key_exp_in_2part,
+            sm4_key_exp_in_3part}    =    sm4_key_exp_in;
+
+assign sm4_key_exp_sbox_in   = sm4_key_exp_in_1part ^ sm4_key_exp_in_2part ^ sm4_key_exp_in_3part ^ sm4_key_cki;
+
+assign    { sm4_key_exp_sbox_in_0part,
+            sm4_key_exp_sbox_in_1part,
+            sm4_key_exp_sbox_in_2part,
+            sm4_key_exp_sbox_in_3part}    =    sm4_key_exp_sbox_in;
+
+assign sm4_key_exp_sbox_out  = {sm4_key_exp_sbox_out_0part, sm4_key_exp_sbox_out_1part, sm4_key_exp_sbox_out_2part, sm4_key_exp_sbox_out_3part};
+
+assign sm4_key_exp_sbox_out_left_shift_13bit = {sm4_key_exp_sbox_out[18:0], sm4_key_exp_sbox_out[31:19]};
+assign sm4_key_exp_sbox_out_left_shift_23bit = {sm4_key_exp_sbox_out[8:0], sm4_key_exp_sbox_out[31:9]};
+assign sm4_key_exp_shift_out = sm4_key_exp_sbox_out ^ sm4_key_exp_sbox_out_left_shift_13bit ^ sm4_key_exp_sbox_out_left_shift_23bit;
+
+assign sm4_rkey_out = sm4_key_exp_shift_out ^ sm4_key_exp_in_0part;
+
+SM4_SBOX U0_SM4_SBOX(
+	.sm4_box_in (sm4_key_exp_sbox_in_0part),
+	.sm4_box_out (sm4_key_exp_sbox_out_0part)
+);
+
+SM4_SBOX U1_SM4_SBOX(
+	.sm4_box_in (sm4_key_exp_sbox_in_1part),
+	.sm4_box_out (sm4_key_exp_sbox_out_1part)
+);
+
+SM4_SBOX U2_SM4_SBOX(
+	.sm4_box_in (sm4_key_exp_sbox_in_2part),
+	.sm4_box_out (sm4_key_exp_sbox_out_2part)
+);
+
+SM4_SBOX U3_SM4_SBOX(
+	.sm4_box_in (sm4_key_exp_sbox_in_3part),
+	.sm4_box_out (sm4_key_exp_sbox_out_3part)
+);
+
+endmodule
